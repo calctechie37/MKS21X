a846bad686634eb1f7a551b37f2782c3f643c198
Patrick Chan <patrickchan1999@gmail.com>
2016-01-04 21:25:07 -0500

    Revised

--- hwSorts/Driver.java
+++ hwSorts/Driver.java
@@ -1,4 +1,10 @@
+import java.util.Arrays;
+
 public class Driver{
     public static void main(String []args){
 	int [] stuff = {1, 9, 2, 7, 3, 10, 8, 5};
-	Sorts.
+	System.out.println(Arrays.toString(stuff));
+	Sorts.selection(stuff);
+	System.out.println(Arrays.toString(stuff));
+    }
+}
