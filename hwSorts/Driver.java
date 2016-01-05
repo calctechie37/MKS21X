c9986cf6ae67988537271a6a408c6621e3d67636
Patrick Chan <patrickchan1999@gmail.com>
2016-01-05 12:22:09 -0500

    Added BubbleSort

--- hwSorts/Driver.java
+++ hwSorts/Driver.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class Driver{
     public static void main(String []args){
-	int [] stuff = {1, 9, 2, 7, 3, 10, 8, 5};
+	int [] stuff = {1,2,9,5,0,3};
 	System.out.println(Arrays.toString(stuff));
-	Sorts.selection(stuff);
+	Sorts.bubbleSort(stuff);
 	System.out.println(Arrays.toString(stuff));
     }
 }
--- hwSorts/Sorts.java
+++ hwSorts/Sorts.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
+
 public class Sorts{
-    public static void selection(int [] data){
+    public static void selectionSort(int [] data){
 	for(int i = 0; i < data.length; i++){
 	    int target = data[i];
 	    int index = i;
@@ -12,6 +14,33 @@ public class Sorts{
 	    int temp = data[i];
 	    data[i] = target;
 	    data[index] = temp;
+	}
+    }
+
+    public static void insertionSort(int[]data){
+	for(int i = 1; i < data.length; i++){
+	    for(int j = i; j > 0 && data[j] < data[j - 1]; j--){
+		int x = data[j - 1];
+		data[j - 1] = data[j];
+		data[j] = x;
+	    }
+	}
+    }
+
+    public static void bubbleSort(int[]data){
+	int target = data.length;
+	boolean changes = true;
+	while (changes){
+	    changes = false;
+	    for(int j = 1; j < target; j ++){
+		if(data[j] < data[j - 1]) {
+		    int temp = data[j];
+		    data[j] = data[j - 1];
+		    data[j - 1] = temp;
+		    changes = true;
+		}
+	    }
+	    target--;
 	}
     }
 }
