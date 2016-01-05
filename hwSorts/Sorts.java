c4af6ca5b3e120bd76cfa6e3f46d0be91cf9390a
Patrick Chan <patrickchan1999@gmail.com>
2016-01-04 21:30:30 -0500

    Revised

--- hwSorts/Sorts.java
+++ hwSorts/Sorts.java
@@ -1,16 +1,18 @@
-public static void selection(int [] data){
-    for(int i = 0; i < data.length; i++){
-	target = data[i];
-	int index;
-	for(int j = i; j < data.length; j++){
-	    if (data[j] < target){
-		target = data[j];
-		index = j;
+public class Sorts{
+    public static void selection(int [] data){
+	for(int i = 0; i < data.length; i++){
+	    int target = data[i];
+	    int index = i;
+	    for(int j = i; j < data.length; j++){
+		if (data[j] < target){
+		    target = data[j];
+		    index = j;
+		}
 	    }
+	    int temp = data[i];
+	    data[i] = target;
+	    data[index] = temp;
 	}
-	int temp = data[i];
-	data[i] = target;
-	data[index] = temp;
     }
 }
 	    
